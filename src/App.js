3fc12d3741587dca22add490e38b487ee4cdc425
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-07-16 10:28:19 -0500

    react port done

--- src/App.js
+++ src/App.js
@@ -4,19 +4,38 @@ import About from "./components/About.js";
 import Contact from "./components/Contact.js";
 import Navbar from "./components/Navbar.js";
 import Projects from "./components/Projects.js";
-import Skills from "./components/Skills";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 
 
 export default function App() {
+
+
+
+
   return (
+
+
+
+    <Router>
+      
     <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
-      <About />
-      <Projects />
-      <Skills />
+       <Route exact path="/" component= {About} />
+      <Route exact path="/Projects" component= {Projects} />
+
       
-      <Contact />
+      
+      <Route exact path="/Contact" component= {Contact} />
     </main>
+
+
+    </Router>
+    
   );
 }
+
+
+
--- src/components/About.js
+++ src/components/About.js
@@ -1,40 +1,22 @@
 import React from "react";
+import Brian from "../Assests/images/0612201116a_HDR.jpg"
 
 export default function About() {
   return (
-    <section id="about">
-      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Greetings, my name is Brian Sales
-            <br className="hidden lg:inline-block" />Building apps is awesome!
-          </h1>
-          <p className="mb-8 leading-relaxed">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
-          </p>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-              Work With Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-              Past Work
-            </a>
-          </div>
+    <section id = "about" >
+    <section class="me">
+
+      <h2>Thank You for visiting</h2>
+      <img src={Brian} alt="Brian Sales holding his cat Selina"/>
+
+</section>
+      <section id="my-story">
+        <h2>My Story</h2>
+        <div>
+          <p>First and foremost I love all 5 of my pets equally. It has been over ten years since undergrad I am finally able to go back to school and learn and new skill.
+            I am a current student at Northwestern University coding bootcamp. I spend my days as a driver for Amazon. I love playing video games and have various horticulture projects.</p>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
-        </div>
-      </div>
+      </section>
     </section>
   );
 }
